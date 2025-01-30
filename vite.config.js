21153104e44c01fa00cb5cd6dc071c6c87016041
ZABocek <zabocek@gmail.com>
2025-01-30 15:32:19 -0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/ZABocek/IBM_GitHub_Repository_Course_5_React",
   plugins: [react()],
 })
